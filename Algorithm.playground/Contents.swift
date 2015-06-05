5b93c6c210eaf18cc284478293cdc164c77937f1
Giang Le Ngoc <giangln@seesaa.vn>
2015-06-05 14:31:19 +0700

    update InsertionSort + QuickSort

--- Algorithm.playground/Contents.swift
+++ Algorithm.playground/Contents.swift
@@ -39,6 +39,75 @@ func binarySearch(key: Int,imin : Int, imax:Int) {
 binarySearch(45, 0, 9)
 
 
+var numberList : Array<Int> = [4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3,4,6,3,5,6,7,3,2,6,6,2,4,6,7,3]
+
+func insertionSort() {
+    var numberList : Array<Int> = [4,6,3,5,6,7,3,2,6,6,2,4,6,7,3]
+    
+    var x,y,key : Int
+    
+    for (x = 0 ; x < numberList.count; x++) {
+        
+        key = numberList[x]
+        
+        for (y = x ;y > -1 ; y--) {
+            
+            if key < numberList[y] {
+                
+                numberList.removeAtIndex(y+1)
+                
+                numberList.insert(key, atIndex: y)
+            }
+        }
+    }
+    println(numberList)
+}
+insertionSort()
+
+
+// Quick sort
+
+func partition(start :Int,end :Int,inout inputArr:Array<Int>) {
+    if (end - start < 2){return}
+    
+    var p = inputArr[start + (end - start)/2]
+    var l = start
+    var r = end - 1
+    
+    while( l <= r ) {
+        if inputArr[l] < p {
+            l += 1
+            continue
+        }
+        if inputArr[r] > p {
+            r -= 1
+            continue
+        }
+        
+        var t = inputArr[l]
+        inputArr[l] = inputArr[r]
+        inputArr[r] = t
+        l += 1
+        r -= 1
+    }
+    partition(start, r+1, &inputArr)
+    partition(r+1, end, &inputArr)
+}
+func quickSort() {
+    partition(0, numberList.count-1, &numberList)
+}
+let swift_start:UInt64 = mach_absolute_time()
+quickSort()
+let swift_stop:UInt64 = mach_absolute_time() - swift_start
+
+var info : mach_timebase_info = mach_timebase_info(numer: 0, denom: 0)
+mach_timebase_info(&info)
+
+let total = (swift_stop * UInt64(info.numer) / UInt64(info.denom)) / 1_000_000
+println("\(total) µs.")
+
+
+
 
 
 
